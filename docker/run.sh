08637b218e509bbf84fb40e3480364822bc6d133
ruffsl <roxfoxpox@gmail.com>
2016-04-01 01:49:41 -0400

    Exposing ports for web caffe

--- docker/run.sh
+++ docker/run.sh
@@ -8,6 +8,9 @@ nvidia-docker \
 	-it \
 	--env="DISPLAY" \
 	--env="QT_X11_NO_MITSHM=1" \
+  --publish 8080:8080 \
+  --publish 8085:8085 \
+  --publish 9090:9090 \
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
   --volume="/home/${USER}/Downloads/data:/root/catkin_ws/src/ros_caffe/ros_caffe/data" \
   --device /dev/video0:/dev/video0 \
