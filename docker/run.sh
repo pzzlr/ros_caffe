5b8d4b64e244f656a2f6c0583954f345a3838b0f
ruffsl <roxfoxpox@gmail.com>
2016-03-31 21:59:00 -0400

    run script tweek for webcam

--- docker/run.sh
+++ docker/run.sh
@@ -10,8 +10,14 @@ nvidia-docker \
 	--env="QT_X11_NO_MITSHM=1" \
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
   --volume="/home/${USER}/Downloads/data:/root/catkin_ws/src/ros_caffe/data" \
-	--device /dev/bus/usb:/dev/bus/usb \
-	--privileged \
+  --device /dev/video0:/dev/video0 \
 	ros:indigo-ros-caffe-dev bash
 
 xhost -local:root
+
+# For openni
+# --device /dev/bus/usb:/dev/bus/usb \
+# --privileged \
+
+# For webcam
+# --device /dev/video0:/dev/video0 \
