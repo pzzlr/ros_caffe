351189f24055f515cd97b6d50873ca893bd44db8
ruffsl <roxfoxpox@gmail.com>
2016-03-31 21:42:59 -0400

    Adding fixes to use webcam

--- docker/run.sh
+++ docker/run.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-docker run \
+xhost +local:root
+GPU=0
+
+nvidia-docker \
+  run \
 	-it \
 	--env="DISPLAY" \
 	--env="QT_X11_NO_MITSHM=1" \
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-	--volume="/home/${USER}/Desktop/caffe:/root/Desktop" \
-	--volume="/lib/modules/:/lib/modules/:ro" \
-	--volume="/etc/modprobe.conf/:/etc/modprobe.conf/:ro" \
-	--device /dev/nvidia0:/dev/nvidia0 \
-	--device /dev/nvidiactl:/dev/nvidiactl \
+  --volume="/home/${USER}/Downloads/data:/root/catkin_ws/src/ros_caffe/data" \
 	--device /dev/bus/usb:/dev/bus/usb \
 	--privileged \
-	$@
+	ros:indigo-ros-caffe-dev bash
+
+xhost -local:root
