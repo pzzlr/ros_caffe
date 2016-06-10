ef7c804cb954a145b32dacd7c7cc643ef34775fc
ruffsl <roxfoxpox@gmail.com>
2016-06-09 17:05:51 -0700

    commenting out dev build by defult

--- docker/run.sh
+++ docker/run.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
 
 xhost +local:root
-GPU=0
 
-nvidia-docker \
-  run \
-	-it \
-	--env="DISPLAY" \
-	--env="QT_X11_NO_MITSHM=1" \
+nvidia-docker run \
+  -it \
+  --env="DISPLAY" \
+  --env="QT_X11_NO_MITSHM=1" \
   --publish 8080:8080 \
   --publish 8085:8085 \
   --publish 9090:9090 \
-	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-  --volume="/home/${USER}/Downloads/data:/root/catkin_ws/src/ros_caffe/ros_caffe/data" \
+  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+  --volume="/home/${USER}/ros_caffe/data:/root/catkin_ws/src/ros_caffe/ros_caffe/data:ro" \
   --device /dev/video0:/dev/video0 \
-	ros:indigo-ros-caffe-dev bash
+  ros:indigo-ros-caffe bash
 
 xhost -local:root
 
